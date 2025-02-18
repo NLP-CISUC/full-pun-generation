4ee5905390a74aa3d7045104e8aff52c67fda255
Marcio Lima Inácio <marcio.lima.inacio@gmail.com>
2025-02-18 17:43:37 +0000

    Final versino of interface with likert scale

--- evaluation_interface/1_🏠_Início.py
+++ evaluation_interface/1_🏠_Início.py
@@ -1,13 +1,20 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 
-st.set_page_config(layout="wide")
 
 authenticator = stauth.Authenticate("config/credentials.yaml")
 try:
     authenticator.login()
 except Exception as e:
     st.error(e)
+
+st.markdown("""
+<style>
+    .stMainBlockContainer {
+        max-width: 850px;
+    }
+</style>
+""", unsafe_allow_html=True)
 
 if st.session_state.authentication_status:
     authenticator.logout("Terminar sessão", "main")
@@ -20,32 +27,38 @@ if st.session_state.authentication_status:
     Essa avaliação esta estuturada da seguinte forma. Você receberá um máximo de 5 noticias, uma por vez, em uma página com o título no topo e 10 piadas listadas abaixo. Cada piada possui duas escalas, correspondentes aos critérios abaixo. Para realizar a avaliação, você deverá selecionar o valor correspondente à sua percepção de acordo com o critério.
 
     As escalas seguem dois critérios diferentes:
-    - **Nível de humor** (escala da **esquerda**, vai de "tem pouca piada" até "tem muita piada");
-    - **Relação com o título** (escala da **direita**, vai de "não tem relação" até "tem muita relação").
+    - **Nível de humor** (escala da **esquerda**)
+        - "Não tem piada" = Não imagino ninguém rindo disso
+        - "Tem pouca piada" = O texto pode fazer sorrir
+        - "Tem piada" = O texto pode fazer rir
+    - **Relação com o título** (escala da **direita**)
+        - "Não relacionado à notícia" = A piada não tem relação alguma com o título ou não faz sentido no contexto
+        - "Pouco relacionado à notícia" = A piada tem pouca relação com o título (por exemplo, alguma palavra-chave)
+        - "Relacionado à notícia" = A piada tem relação com o tema geral do título
 
     Aqui embaixo você verá um pequeno exemplo (com 3 piadas) para entender como é a interface de avaliação, pode brincar à vontade! Se tiver qualquer dúvida, pode vir falar comigo.
     """)
 
     joke_examples = ["Piada 1", "Piada 2", "Piada 3"]
-    with st.expander("Avaliação de teste", expanded=True):
-        st.title("ChatGPT é visto dirigindo Uber após chegada de IA chinesa")
-        for i in range(len(joke_examples)):
-            with st.container(border=True):
-                st.markdown(f"<p style=\"font-size:21px\">{joke_examples[i]}</p>",
-                            unsafe_allow_html=True)
-                col1, col2 = st.columns(2, gap="large")
-                with col1:
-                    st.select_slider("Qual o nível de piada?",
-                                     ["Não tem piada", "Tem pouca piada",
-                                      "Tem piada", "Tem muita piada"],
-                                     key=f"test_slider_fun_{i}",
-                                     label_visibility="hidden")
-                with col2:
-                    st.select_slider("A piada tem relação com a notícia?",
-                                     ["Não tem relação", "Tem pouca relação",
-                                      "Tem relação", "Tem muita relação"],
-                                     key=f"test_slider_rel_{i}",
-                                     label_visibility="hidden")
+    st.header("ChatGPT é visto dirigindo Uber após chegada de IA chinesa")
+    for i in range(len(joke_examples)):
+        with st.container(border=True):
+            st.markdown(f"<p style=\"font-size:19px\">{joke_examples[i]}</p>",
+                        unsafe_allow_html=True)
+            col1, col2 = st.columns(2)
+            with col1:
+                st.pills("1\. Qual o nível de piada?",
+                         ["Não tem piada", "Tem pouca piada", "Tem piada"],
+                         default="Não tem piada",
+                         key=f"test_slider_fun_{i}")
+            with col2:
+                st.pills("2\. A piada tem relação com a notícia?",
+                         ["Não tem relação",
+                          "Tem pouca relação",
+                          "Tem relação"],
+                         default="Não tem relação",
+                         key=f"test_slider_rel_{i}")
+
     st.markdown("Quando estiver pronto(a) para começar, é só ir para a página Avaliação no menu lateral (à esquerda). Boa avaliação! :smile:")
 elif st.session_state.authentication_status == False:
     st.error("Usuário/senha incorreta")
--- evaluation_interface/pages/2_✍️_Avaliação.py
+++ evaluation_interface/pages/2_✍️_Avaliação.py
@@ -19,20 +19,22 @@ df = load_data().filter(pl.col("headline_id").is_in(splits[username]))
 
 
 def format_headline():
-    headline_id = st.session_state.cur_headline
-    return df.filter(pl.col("headline_id") == headline_id)["headline"].first()
+    cur_headline = st.session_state.cur_headline
+    return df.filter(pl.col("headline_id") == cur_headline)["headline"].first()
 
 
 def update_jokes():
     cur_headline = st.session_state.cur_headline
-    if cur_headline in st.session_state.jokes_by_headline:
-        jokes = st.session_state.jokes_by_headline[cur_headline]
+    if ("results_df" in st.session_state and
+        cur_headline in st.session_state.results_df["headline_id"]):
+        jokes = (st.session_state.results_df
+                 .filter(pl.col("headline_id") == cur_headline)["generated"]
+                 .to_list())
     else:
         jokes = (df.filter(pl.col("headline_id") == cur_headline)["generated"]
                  .unique()
                  .sample(fraction=1, shuffle=True)
                  .to_list())
-        st.session_state.jokes_by_headline[cur_headline] = jokes
     st.session_state.cur_jokes = jokes
 
 
@@ -59,16 +61,13 @@ def set_show_success():
 
 
 def update_rates():
-    fun_rates, rel_rates = [], []
+    jokes = {}
     for i in range(len(st.session_state.cur_jokes)):
-        fun = st.session_state[f"slider_fun_{st.session_state.cur_headline}_{i}"]
-        rel = st.session_state[f"slider_rel_{st.session_state.cur_headline}_{i}"]
-        fun_rates.append(fun)
-        rel_rates.append(rel)
-    st.session_state.rates[st.session_state.cur_headline] = {
-        "funniness": fun_rates,
-        "relation": rel_rates
-    }
+        fun = st.session_state[f"funniness_{st.session_state.cur_headline}_{i}"]
+        rel = st.session_state[f"relation_{st.session_state.cur_headline}_{i}"]
+        jokes[st.session_state.cur_jokes[i]] = {"funniness": fun, "relation": rel}
+
+    st.session_state.rates[st.session_state.cur_headline] = jokes
 
 
 #
@@ -78,6 +77,13 @@ if "rates" not in st.session_state:
     st.session_state.rates = {}
 if "results_df" not in st.session_state and results_path.exists():
     st.session_state.results_df = pl.read_ndjson(results_path)
+    for row in st.session_state.results_df.iter_rows(named=True):
+        if row["headline_id"] not in st.session_state.rates:
+            st.session_state.rates[row["headline_id"]] = {}
+        st.session_state.rates[row["headline_id"]][row["generated"]] = {
+            "funniness": row["funniness"],
+            "relation": row["relation"]
+        }
 if "cur_idx" not in st.session_state:
     st.session_state.cur_idx = 0
 if "cur_headline" not in st.session_state:
@@ -86,8 +92,6 @@ if "show_success" not in st.session_state:
     st.session_state.show_success = False
 if "scroll_to_top" not in st.session_state:
     st.session_state.scroll_to_top = False
-if "jokes_by_headline" not in st.session_state:
-    st.session_state.jokes_by_headline = {}
 if "cur_jokes" not in st.session_state:
     update_jokes()
 
@@ -112,7 +116,7 @@ if st.session_state.show_success:
     st.stop()
 
 header = st.container()
-header.title(format_headline(), anchor="headline")
+header.header(format_headline(), anchor="headline")
 header.write("""<div class='fixed-header'/>""", unsafe_allow_html=True)
 st.markdown(
     """
@@ -126,6 +130,12 @@ st.markdown(
     .fixed-header {
         border-bottom: 1px solid #333333;
     }
+    .stMainBlockContainer {
+        max-width: 850px;
+    }
+    li::marker {
+        font-size: 19px;
+    }
 </style>
     """,
     unsafe_allow_html=True
@@ -133,32 +143,34 @@ st.markdown(
 
 for i in range(len(st.session_state.cur_jokes)):
     try:
-        default_fun = st.session_state.rates[st.session_state.cur_headline]["funniness"][i]
-        default_rel = st.session_state.rates[st.session_state.cur_headline]["relation"][i]
+        cur_headline = st.session_state.cur_headline
+        cur_joke = st.session_state.cur_jokes[i]
+        default_fun = st.session_state.rates[cur_headline][cur_joke]["funniness"]
+        default_rel = st.session_state.rates[cur_headline][cur_joke]["relation"]
     except (KeyError, IndexError):
         default_fun = "Não tem piada"
         default_rel = "Não tem relação"
 
     with st.container(border=True):
-        st.markdown(f"<p style=\"font-size:21px\">{st.session_state.cur_jokes[i]}</p>",
+        st.markdown(f"<p style=\"font-size:19px\">{st.session_state.cur_jokes[i]}</p>",
                     unsafe_allow_html=True)
-        col1, col2 = st.columns(2, gap="large")
+        col1, col2 = st.columns(2)
         with col1:
-            st.select_slider("Qual o nível de piada?",
-                             ["Não tem piada", "Tem pouca piada",
-                              "Tem piada", "Tem muita piada"],
-                             value=default_fun,
-                             key=f"slider_fun_{st.session_state.cur_headline}_{i}",
-                             label_visibility="hidden",
-                             on_change=update_rates)
+            st.pills("1\. Qual o nível de piada?",
+                     ["Não tem piada", "Tem pouca piada",
+                      "Tem piada"],
+                     default=default_fun,
+                     key=f"funniness_{st.session_state.cur_headline}_{i}",
+                     on_change=update_rates)
         with col2:
-            st.select_slider("A piada tem relação com a notícia?",
-                             ["Não tem relação", "Tem pouca relação",
-                              "Tem relação", "Tem muita relação"],
-                             value=default_rel,
-                             key=f"slider_rel_{st.session_state.cur_headline}_{i}",
-                             label_visibility="hidden",
-                             on_change=update_rates)
+            st.pills("2\. A piada tem relação com a notícia?",
+                     ["Não tem relação", "Tem pouca relação",
+                      "Tem relação"],
+                     default=default_rel,
+                     key=f"relation_{st.session_state.cur_headline}_{i}",
+                     on_change=update_rates)
+
+    st.container(height=25, border=False)
 
 btn_col1, _, btn_col2 = st.columns([1, 3, 1])
 with btn_col1:
@@ -174,17 +186,24 @@ with btn_col2:
         st.button("Concluir", type="primary", on_click=set_show_success,
                   use_container_width=True)
 
-
 #
 # ------ Consolidate and save results ------
 #
-if st.session_state.rates:
-    rates = [(pl.DataFrame(st.session_state.rates[headline_id])
-              .with_columns(pl.lit(headline_id).alias("headline_id"),
-                            pl.lit(username).alias("evaluator"),
-                            pl.Series(st.session_state.jokes_by_headline[headline_id]).alias("generated")))
-             for headline_id in st.session_state.rates]
-    rates = pl.concat(rates)
-
-    st.session_state.results_df = df.join(rates, on=["headline_id", "generated"])
+if st.session_state.rates and st.session_state.cur_headline in st.session_state.rates:
+    rates = {"evaluator": [], "headline_id": [], "generated": [], "funniness": [], "relation": []}
+    for headline_id in st.session_state.rates:
+        for generated in st.session_state.rates[headline_id]:
+            rates["evaluator"].append(username)
+            rates["headline_id"].append(headline_id)
+            rates["generated"].append(generated)
+            rates["funniness"].append(st.session_state.rates[headline_id][generated]["funniness"])
+            rates["relation"].append(st.session_state.rates[headline_id][generated]["relation"])
+    rates_df = pl.DataFrame(rates)
+    rates_df = df.join(rates_df, on=["headline_id", "generated"])
+    if "results_df" in st.session_state:
+        rates_df = (st.session_state.results_df.update(rates_df,
+                                                       on=["headline_id",
+                                                           "generated"],
+                                                       how="full"))
+    st.session_state.results_df = rates_df
     st.session_state.results_df.write_ndjson(results_path)
