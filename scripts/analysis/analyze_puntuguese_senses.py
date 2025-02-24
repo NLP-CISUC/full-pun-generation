05b1334f1a63b41a0159b83ee4bfbe16ac5e025b
Marcio Lima Inácio <marcio.lima.inacio@gmail.com>
2025-02-24 11:18:16 +0000

    Puntuguese analysis with altair

--- scripts/analysis/analyze_puntuguese_senses.py
+++ scripts/analysis/analyze_puntuguese_senses.py
@@ -5,6 +5,7 @@ import seaborn as sns
 from full_pun_generation.puntuguese import Puntuguese
 from full_pun_generation.wordnet import (get_words_synsets,
                                          get_definitions_similarity)
+import altair as alt
 
 
 def get_similarities(words):
@@ -18,7 +19,7 @@ def get_similarities(words):
     return pl.Series(min_similarities)
 
 
-puntuguese = Puntuguese("../Puntuguese/data/puns.json")
+puntuguese = Puntuguese("../../Resources/Corpora/Puntuguese/data/puns.json")
 puntuguese.filter_data()
 homographs = (puntuguese.train.filter(pl.col("homograph"))
               .with_columns(
@@ -28,7 +29,14 @@ homographs = (puntuguese.train.filter(pl.col("homograph"))
               .filter(~pl.col("similarity").is_infinite())
               )
 
-print(homographs.select(pl.col("similarity")).describe())
+print(homographs.select(pl.col("similarity"))
+      .describe(percentiles=[0.25, 0.5, 0.7, 0.75]))
 
-sns.histplot(data=homographs, x="similarity")
-plt.savefig("results/img/puntuguese_similarity_dist.png")
+base = alt.Chart(homographs)
+histogram = (base.mark_bar().encode(x=alt.X("similarity:Q").bin(),
+                                    y="count()"))
+rule = base.mark_rule(color="red").encode(x=alt.X(datum=0.2),
+                                          size=alt.value(2))
+chart = histogram + rule
+chart.save("results/img/puntuguese_similarity_dist.png", ppi=300)
+
