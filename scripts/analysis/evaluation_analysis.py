7aae48cc2a36eadee4a60dfa2ea728131414de83
Marcio Lima Inácio <marcio.lima.inacio@gmail.com>
2025-02-25 00:56:15 +0000

    More analysis for the paper

--- scripts/analysis/evaluation_analysis.py
+++ scripts/analysis/evaluation_analysis.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
 from pathlib import Path
 
+from polars.convert import normalize
+
 import altair as alt
 import krippendorff
 import numpy as np
@@ -29,6 +31,8 @@ df = (pl.concat(dfs)
       .select([pl.col("evaluator"),
                pl.col("model").str.replace_many(model_names),
                pl.col("headline_id"),
+               pl.col("headline"),
+               pl.col("generated"),
                pl.col("funniness"),
                pl.col("typicality"),
                pl.col("relation"),
@@ -41,72 +45,140 @@ new_rows = list()
 splits_path = Path("evaluation_interface/config/config.yaml")
 splits = yaml.safe_load(splits_path.read_text())["splits"]
 splits_df = (pl.DataFrame([{"evaluator": evaluator, "headline_id": headline_ids}
-                          for evaluator, headline_ids in splits.items()
-                          if evaluator in df["evaluator"].unique()])
+                           for evaluator, headline_ids in splits.items()
+                           if evaluator in df["evaluator"].unique()])
              .explode("headline_id"))
 splits_df = splits_df.join(df.select(pl.col("model")), how="cross").unique()
 missing_df = (splits_df.join(df, on=["evaluator", "headline_id", "model"], how="anti")
               .with_columns([pl.lit("Não tem piada").alias("funniness"),
                              pl.lit("Não tem relação").alias("relation")])
-              .join(df.select(["headline_id", "model", "typicality", "similarity"]).unique(),
+              .join(df.select(pl.all().exclude(["evaluator", "funniness", "relation"])).unique(),
                     on=["headline_id", "model"], how="left")
               .select(pl.col("evaluator"),
                       pl.col("model"),
                       pl.col("headline_id"),
+                      pl.col("headline"),
+                      pl.col("generated"),
                       pl.col("funniness"),
                       pl.col("typicality"),
                       pl.col("relation"),
                       pl.col("similarity")))
 print(f"Added {missing_df.height} missing rows.\n")
-df = df.extend(missing_df)
+df = (df.extend(missing_df)
+      .with_columns(pl.col("funniness").cast(pl.Enum(fun_order)).to_physical() + 1,
+                    pl.col("relation").cast(pl.Enum(rel_order)).to_physical() + 1))
 
 # Descriptive statistics
-df = (df.with_columns([pl.col("funniness").cast(pl.Enum(fun_order)).to_physical(),
-                       pl.col("relation").cast(pl.Enum(rel_order)).to_physical()]))
 agg_df = (df.group_by(["model", "headline_id"])
-          .agg(pl.col("funniness").to_physical().mode().get(0),
+          .agg(pl.col("funniness").mode().max(),
                pl.col("typicality").first(),
-               pl.col("relation").to_physical().mode().get(0),
+               pl.col("relation").mode().max(),
                pl.col("similarity").first()))
-agg_df = df
 
-sort_fun = df.group_by("model").mean().sort("funniness", descending=True)
-sort_rel = df.group_by("model").mean().sort("relation", descending=True)
+
+print("###### Descriptive statistics ######")
+
+def tastle_wierman_consensus(x: pl.Series) -> float:
+    x_idx = x.to_numpy()
+    categories, counts = np.unique(x_idx, return_counts=True)
+    k = len(categories)
+    n = len(x_idx)
+
+    if k == 1:
+        return 1.0
+
+    mu_x = np.sum(categories * counts) / n
+    p_i = counts / n
+    d_x = k - 1
+    term = p_i * np.log2(1 - np.abs(categories - mu_x) / d_x)
+    consensus = 1 + term.sum()
+    return consensus
+
+print(df.group_by("model").agg(pl.col("funniness")
+                               .quantile(0.5).alias("funniness median"),
+                               pl.col("funniness")
+                               .map_batches(tastle_wierman_consensus)
+                               .alias("funniness consensus").get(0),
+                               pl.col("relation")
+                               .quantile(0.5).alias("relation median"),
+                               pl.col("relation")
+                               .map_batches(tastle_wierman_consensus)
+                               .alias("relation consensus").get(0))
+      .sort("model")
+      .write_csv())
 
 # Funniness, Typicality, Relation, and Similarity correlation
 print("###### Correlation analysis ######")
-print(agg_df.select(pl.col("funniness"),
-                    pl.col("typicality"),
-                    pl.col("relation"),
-                    pl.col("similarity")).corr())
+print(df.select(pl.col("funniness"),
+                pl.col("typicality"),
+                pl.col("relation"),
+                pl.col("similarity")).corr())
 print()
 
+# Krippendorff's alpha
 print("###### Agreement analysis ######")
-# Krippendorff's alpha for funniness
-fun_data = (df.select("evaluator")
-            .join(df.select("model", "headline_id"), how="cross").unique()
-            .join(df, on=["evaluator", "model", "headline_id"], how="left")
-            .pivot(index="evaluator", on=["model", "headline_id"], values="funniness")
-            .drop("evaluator")
-            .to_numpy())
-fun_alpha = krippendorff.alpha(fun_data, level_of_measurement="ordinal")
-print(f"Krippendorff's alpha for funniness: {fun_alpha:.2f}")
+models = df["model"].unique()
+funniness_alpha, relation_alpha = list(), list()
+for model in models:
+    fun_data = (df.filter(pl.col("model") == model)
+                .select("evaluator", "model")
+                .join(df.select("headline_id"), how="cross").unique()
+                .join(df, on=["evaluator", "model", "headline_id"], how="left")
+                .drop_nulls()
+                .pivot(index="evaluator", on="headline_id", values="funniness")
+                .drop("evaluator"))
+    fun_alpha = krippendorff.alpha(fun_data, level_of_measurement="ordinal")
+    funniness_alpha.append(fun_alpha)
 
-# Krippendorff's alpha for relation
-rel_data = (df.select("evaluator")
-            .join(df.select("model", "headline_id"), how="cross").unique()
-            .join(df, on=["evaluator", "model", "headline_id"], how="left")
-            .pivot(index="evaluator", on=["model", "headline_id"], values="relation")
-            .drop("evaluator")
-            .to_numpy())
-rel_alpha = krippendorff.alpha(rel_data, level_of_measurement="ordinal")
-print(f"Krippendorff's alpha for relation: {rel_alpha:.2f}")
+    rel_data = (df.filter(pl.col("model") == model)
+                .select("evaluator", "model")
+                .join(df.select("headline_id"), how="cross").unique()
+                .join(df, on=["evaluator", "model", "headline_id"], how="left")
+                .drop_nulls()
+                .pivot(index="evaluator", on="headline_id", values="relation")
+                .drop("evaluator"))
+    rel_alpha = krippendorff.alpha(rel_data, level_of_measurement="ordinal")
+    relation_alpha.append(rel_alpha)
+
+
+alpha = pl.DataFrame({"model": models, "funniness alpha": funniness_alpha,
+                      "relation alpha": relation_alpha})
+print(alpha.write_csv())
+
+# Llama3.3+shot Relation disagreements
+print("###### Llama3.3+shot relation disagreements ######")
+headlines = df["headline_id"].unique()
+num_disagreements = 0
+for headline_id in headlines:
+    llama_shot = (df.filter(pl.col("model") == "Llama3.3+shot")
+                  .filter(pl.col("headline_id") == headline_id))
+    if llama_shot["relation"].n_unique() > 1:
+        num_disagreements += 1
+    if llama_shot["relation"].n_unique() > 2:
+        headline = llama_shot["headline"].first()
+        joke = llama_shot["generated"].first()
+        ratings = llama_shot.select("evaluator", "relation").sort("evaluator")
+        print(f"Headline: {headline}")
+        print(f"Joke: {joke}")
+        print(f"Ratings: {', '.join([rel_order[r - 1] for r in ratings['relation']])}")
+        print()
+print(f"Total disagreements in Llama3.3+shot: {num_disagreements}")
+
+# Best jokes for every model
+print("###### Best jokes for every model ######")
+best_jokes = (agg_df.filter(pl.col("funniness") == 3)
+              .group_by("model").agg(pl.col("headline_id").sample(1).get(0))
+              .join(df, on=["model", "headline_id"])
+              .select("model", "headline", "generated")
+              .unique())
+print(best_jokes.write_csv())
 
 if not args.charts:
     exit()
 
 # Funniness
-fun_base = (alt.Chart(agg_df, title=alt.Title("Funniness", dy=-10))
+sort_fun = df.group_by("model").mean().sort("funniness", descending=True)
+fun_base = (alt.Chart(df, title=alt.Title("Funniness", dy=-10))
             .mark_bar(size=20)
             .encode(alt.X("count()")
                     .stack("normalize")
@@ -123,7 +195,7 @@ fun_base = (alt.Chart(agg_df, title=alt.Title("Funniness", dy=-10))
             .properties(width=500, height=300))
 fun_bars = (fun_base.mark_bar()
             .encode(color=alt.Color("funniness:O")
-                    .scale(domain=[0, 1, 2], range=bar_color_scheme)
+                    .scale(domain=[1, 2, 3], range=bar_color_scheme)
                     .legend(None)))
 ratio_fun_text = (fun_base.transform_joinaggregate(count="count()",
                                                    groupby=["model", "funniness"])
@@ -134,7 +206,7 @@ ratio_fun_text = (fun_base.transform_joinaggregate(count="count()",
                              fontWeight="bold")
                   .encode(text=alt.Text("ratio:Q", format=".0%"),
                           color=alt.Color("funniness:O")
-                          .scale(domain=[0, 1, 2], range=text_color_scheme)
+                          .scale(domain=[1, 2, 3], range=text_color_scheme)
                           .legend(None)))
 left_fun_text = (fun_bars
                  .mark_text(size=11, align="left",
@@ -161,7 +233,8 @@ fun_c = (alt.layer(fun_bars, left_fun_text, middle_fun_text, right_fun_text, rat
 fun_c.save("results/img/funniness_dist.png", ppi=300)
 
 # Relation
-rel_base = (alt.Chart(agg_df, title=alt.Title("Relation", dy=-10))
+sort_rel = df.group_by("model").mean().sort("relation", descending=True)
+rel_base = (alt.Chart(df, title=alt.Title("Relation", dy=-10))
             .encode(alt.X("count()")
                     .stack("normalize")
                     .axis(alt.Axis(domain=False,
@@ -178,7 +251,7 @@ rel_base = (alt.Chart(agg_df, title=alt.Title("Relation", dy=-10))
             .properties(width=500, height=300))
 rel_bars = (rel_base.mark_bar()
             .encode(color=alt.Color("relation:O")
-                    .scale(domain=[0, 1, 2], range=bar_color_scheme)
+                    .scale(domain=[1, 2, 3], range=bar_color_scheme)
                     .legend(None)))
 ratio_rel_text = (rel_base.transform_joinaggregate(count="count()",
                                                    groupby=["model", "relation"])
@@ -189,7 +262,7 @@ ratio_rel_text = (rel_base.transform_joinaggregate(count="count()",
                              fontWeight="bold")
                   .encode(text=alt.Text("ratio:Q", format=".0%"),
                           color=alt.Color("relation:O")
-                          .scale(domain=[0, 1, 2], range=text_color_scheme)
+                          .scale(domain=[1, 2, 3], range=text_color_scheme)
                           .legend(None)))
 left_rel_text = (rel_bars
                  .mark_text(size=11, align="left",
